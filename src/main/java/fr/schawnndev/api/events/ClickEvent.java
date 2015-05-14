7db1b671501ad8c06e5157cbedafb1a4fe66b40f
SchawnnDev <iperskill@gmail.com>
2015-05-14 16:41:14 +0200

    Added interface, minimal changes

---
+++ src/main/java/fr/schawnndev/api/events/ClickEvent.java
@@ -0,0 +1,16 @@
+/*
+ * ******************************************************
+ *  * Copyright (C) 2015 SchawnnDev <contact@schawnndev.fr>
+ *  *
+ *  * This file (fr.schawnndev.api.events.ClickEvent) is part of LCCosmetiques.
+ *  *
+ *  * Created by SchawnnDev on 14/05/15 16:33.
+ *  *
+ *  * LCCosmetiques can not be copied and/or distributed without the express
+ *  * permission of SchawnnDev.
+ *  ******************************************************
+ */
+
+package fr.schawnndev.api.events;
+
+public class ClickEvent {}
---
+++ src/main/java/fr/schawnndev/api/interfaces/Click.java
@@ -0,0 +1,22 @@
+/*
+ * ******************************************************
+ *  * Copyright (C) 2015 SchawnnDev <contact@schawnndev.fr>
+ *  *
+ *  * This file (fr.schawnndev.api.interfaces.Click) is part of LCCosmetiques.
+ *  *
+ *  * Created by SchawnnDev on 14/05/15 16:39.
+ *  *
+ *  * LCCosmetiques can not be copied and/or distributed without the express
+ *  * permission of SchawnnDev.
+ *  ******************************************************
+ */
+
+package fr.schawnndev.api.interfaces;
+
+import fr.schawnndev.api.events.ClickEvent;
+
+public interface Click {
+
+    void onClick(ClickEvent e);
+
+}
